98a913ed07cec44ff96f0cac1927f5251cc81cf8
yukki373 <78712648+yukki373@users.noreply.github.com>
2021-03-31 17:36:43 +0900

    新規登録できるようにする

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :gender
+  end
+
+  enum gender: { man: 0, woman: 1 }
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+end
