6672a5e1a13dd1c9d9ad40447652932832baeff5
yukki373 <78712648+yukki373@users.noreply.github.com>
2021-04-04 03:26:52 +0900

    プロフィール編集機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ApplicationRecord
 
   enum gender: { man: 0, woman: 1 }
 
-  
+  mount_uploader :profile_image, ProfileImageUploader
+
   def update_without_current_password(params, *options)
 
     if params[:password].blank? && params[:password_confirmation].blank?
