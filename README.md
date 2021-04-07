5b23e769447dcabed6884fe5ad68af6e2ef4db35
yukki373 <78712648+yukki373@users.noreply.github.com>
2021-04-07 18:22:21 +0900

    チャットルーム作成機能の実装

--- README.md
+++ README.md
@@ -14,6 +14,8 @@
 ### Association
 
 - has_many :reactions
+- has_many :chat_room_users
+- has_many :chat_rooms, through: :chat_room_users
 
 ## reactions テーブル
 
@@ -26,3 +28,25 @@
 ### Association
 
 - belongs_to :user
+
+## chat_room_users テーブル
+
+| Column                   | Type       | Options                       |
+| ------------------------ | ---------- | ------------------------------|
+| chat_room_id             | references | null: false foreign_key: true |
+| user_id                  | references | null: false foreign_key: true |
+
+### Association
+
+- belongs_to :user
+- belongs_to :chat_room
+
+## chat_rooms テーブル
+
+| Column                   | Type       | Options                       |
+| ------------------------ | ---------- | ------------------------------|
+|                          | references | null: false foreign_key: true |
+
+### Association
+
+- mas_many :chat_room_users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_173500) do
+ActiveRecord::Schema.define(version: 2021_04_06_183804) do
+
+  create_table "chat_room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "chat_room_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_room_id"], name: "index_chat_room_users_on_chat_room_id"
+    t.index ["user_id"], name: "index_chat_room_users_on_user_id"
+  end
+
+  create_table "chat_rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reactions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "from_user_id", null: false
@@ -38,6 +52,8 @@ ActiveRecord::Schema.define(version: 2021_04_05_173500) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chat_room_users", "chat_rooms"
+  add_foreign_key "chat_room_users", "users"
   add_foreign_key "reactions", "users", column: "from_user_id"
   add_foreign_key "reactions", "users", column: "to_user_id"
 end
